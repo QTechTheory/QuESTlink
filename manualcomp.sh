6c0e7e9ef32e3fd74e2d3eb4f5161994579f5a21
TysonRayJones <tyson.jones.input@gmail.com>
2018-12-14 15:23:47 +0000

    renamed files

--- manualcomp.sh
+++ manualcomp.sh
@@ -11,21 +11,21 @@ gcc-8 -x c -O2 -std=c99 -mavx -Wall -DQuEST_PREC=2  -c QuEST/CPU/QuEST_cpu_local
 
 ## compile MMA functions
 echo "Compiling Mathematica wrappers..."
-gcc-8 -x c -O2 -std=c99 -mavx -Wall -arch x86_64 -DQuEST_PREC=2  -IQuEST/CPU -IQuEST -c quest_wrapper.c
+gcc-8 -x c -O2 -std=c99 -mavx -Wall -arch x86_64 -DQuEST_PREC=2  -IQuEST/CPU -IQuEST -c quest_link.c
 
 ## generate template sources
 echo "Generating template sources..."
-./WSTPlibs/wsprep mytest.tm -o mytest.tm.c
+./WSTPlibs/wsprep quest_templates.tm -o quest_templates.tm.c
 echo "Compiling template sources..."
-gcc-8 -arch x86_64 -c -o mytest.o mytest.tm.c
+gcc-8 -arch x86_64 -c -o quest_templates.o quest_templates.tm.c
 
 ## link
 echo "Linking QuEST, wrappers and templates..."
-gcc-8 -O2 -std=c99 -mavx -Wall -arch x86_64 -DQuEST_PREC=2  -IQuEST/CPU -IQuEST -o quest_link mytest.o QuEST.o QuEST_validation.o QuEST_common.o QuEST_qasm.o mt19937ar.o QuEST_cpu.o QuEST_cpu_local.o quest_wrapper.o -lm -lc++ WSTPlibs/libWSTPi4.36.a -framework Foundation
+gcc-8 -O2 -std=c99 -mavx -Wall -arch x86_64 -DQuEST_PREC=2  -IQuEST/CPU -IQuEST -o quest_link quest_link.o quest_templates.o QuEST.o QuEST_validation.o QuEST_common.o QuEST_qasm.o mt19937ar.o QuEST_cpu.o QuEST_cpu_local.o -lm -lc++ WSTPlibs/libWSTPi4.36.a -framework Foundation
 
 # cleanup
 echo "Cleaning up..."
-rm mytest.tm.c
+rm quest_templates.tm.c
 rm *.o
 
 echo "Done!"
