a6980fd58191541bf65bdcc3e5bad1c11bba01fa
TysonRayJones <tyson.jones.input@gmail.com>
2020-04-26 18:12:14 +0100

    patched DestroyQureg
    so that any expression ultimately evaluating to an integer can be passed. Previously, intermediate forms of some expressions (like a list Part) were unrecognised by the check determining whether to Clear the passed Symbol

--- Link/QuESTlink.m
+++ Link/QuESTlink.m
@@ -345,12 +345,14 @@ P[outcomes] is a (normalised) projector onto the given {0,1} outcomes. The left 
         Operator[op_?isOperatorFormat] :=
             Reverse @ Circuit @ op
 
-        (* destroying a qureg, and clearing the local symbol *)
+        (* destroying a qureg, and clearing the local symbol if recognised *)
         SetAttributes[DestroyQureg, HoldAll];
         DestroyQureg[qureg_Integer] :=
         	DestroyQuregInternal[qureg]
         DestroyQureg[qureg_Symbol] :=
         	Block[{}, DestroyQuregInternal[ReleaseHold@qureg]; Clear[qureg]]
+        DestroyQureg[qureg_] :=
+            DestroyQuregInternal @ ReleaseHold @ qureg
         DestroyQureg[___] := invalidArgError[DestroyQureg]
 
         (* get a local matrix representation of the qureg. GetQuregMatrixInternal provided by WSTP *)
