478633444f28289c9158cc90e6ce8218783ac80c
TysonRayJones <tyson.jones.input@gmail.com>
2023-03-08 20:38:40 +0000

    added final time in circuit schedules

--- Link/QuESTlink.m
+++ Link/QuESTlink.m
@@ -2669,6 +2669,11 @@ The probability of the forced measurement outcome (if hypothetically not forced)
                 {sub, subcircs}
             ];
             
+            (* append the final time to the end of the schedule *)
+            AppendTo[subcircTimes, curTime];
+            AppendTo[subcircActives, {}];
+            AppendTo[subcircPassives, {}];
+            
             (* return *)    
             {subcircTimes, subcircActives, subcircPassives}
         ]
@@ -2807,7 +2812,7 @@ The probability of the forced measurement outcome (if hypothetically not forced)
             Catch[
                 With[
                     {times = First @ getSchedAndNoiseFromSubcircs[cols,spec]}, (* throws *)
-                    Transpose[{times, cols}] // optionalReplaceAliases[OptionValue[ReplaceAliases], spec]]]
+                    Transpose[{times, Append[cols,{}]}] // optionalReplaceAliases[OptionValue[ReplaceAliases], spec]]]
         GetCircuitSchedule[circ_List, spec_Association, opts:OptionsPattern[]] :=
             GetCircuitSchedule[GetCircuitColumns[circ], spec, opts]
         GetCircuitSchedule[___] := invalidArgError[GetCircuitSchedule]
